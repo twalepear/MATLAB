f6134d9582ae01e9a1712beec6830f5d9cfe9b6a
twalepear <145573584+twalepear@users.noreply.github.com>
2026-03-24 23:32:51 +1030

    Delete week1 directory

--- week1/week1_video5_cmConverter.m
+++
@@ -1,8 +0,0 @@
-clc
-clear
-
-itemName = input("Enter the name of the item: ","s");
-inches = input("Enter the length in inches: ");
-centimetres = inches * 2.54;
-disp([itemName, "measured in centimetres is:"]);
-disp(centimetres);
